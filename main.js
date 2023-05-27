a6107bc774e78adc66ce27381c2fdc907870dffc
saririvaii <snurfauziyya@student.unimelb.edu.au>
2023-05-28 01:35:19 +1000

    Homepage fully responsive & added search bar

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
-// // Hover effect on menu category buttons
-// document.addEventListener('DOMContentLoaded', function() {
-//     var catButtons = document.querySelectorAll('.cat-button p');
-//     catButtons.forEach(function(button) {
-//       button.addEventListener('mouseenter', function() {
-//         this.classList.add('hover');
-//       });
-//       button.addEventListener('mouseleave', function() {
-//         this.classList.remove('hover');
-//       });
-//     });
-// });
+// Hover effect on menu category buttons
+document.addEventListener('DOMContentLoaded', function() {
+    var catButtons = document.querySelectorAll('.cat-button p');
+    catButtons.forEach(function(button) {
+      button.addEventListener('mouseenter', function() {
+        this.classList.add('hover');
+      });
+      button.addEventListener('mouseleave', function() {
+        this.classList.remove('hover');
+      });
+    });
+});
 
 paybutton = document.querySelectorAll('.dot');
 
