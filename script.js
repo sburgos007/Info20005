a7d78c9c2c5b2dc5079a6d0a42cf3c9fd31a3d63
saririvaii <snurfauziyya@student.unimelb.edu.au>
2023-06-01 16:16:59 +1000

    added ABOUT header in view js & finetuned
    finetuning borders, margins, font size

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// Hamburger menu display on toggle
+// Hamburger menu display on toggle ---------------------------------------
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 const aboutbtn = document.querySelector('.about');
@@ -10,6 +10,7 @@ menu_btn.addEventListener('click', function () {
   mobile_menu.classList.toggle('active');
 });
 
+//  Validation of forms ---------------------------------------------------
 function isInputNumber(evt){
   var ch = String.fromCharCode(evt.which);
   if(!(/[0-9]/.test(ch))){
@@ -17,6 +18,42 @@ function isInputNumber(evt){
   }
 }
 
+
+// About us header change -------------------------------------------------
+// Get the elements
+const homeLink = document.querySelector('header .navbar li:nth-child(1) a');
+const aboutLink = document.querySelector('header .navbar li:nth-child(2) a');
+
+
+// Function to check if element is in viewport
+const isInViewport = (element) => {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+// Function to handle scroll event on menu
+const handleScroll = () => {
+  const aboutSection = document.getElementById('aboutSection');
+  if (isInViewport(aboutSection)) {
+    aboutLink.classList.add('active');
+    homeLink.classList.remove('active');
+  } else {
+    aboutLink.classList.remove('active');
+    homeLink.classList.add('active');
+  }
+};
+
+window.addEventListener('scroll', handleScroll);
+
+
+
+
+// Add and minus buttons --------------------------------------------------
 function plus() {
   document.getElementById('number').stepUp();
 }
@@ -39,8 +76,7 @@ function minus3() {
 }
 
 
-
-// Change background image every 5 seconds
+// Change background image every 5 seconds ---------------------------------
 let images = document.querySelectorAll('.page1-container img');
 let currentImageIndex = 0;
 
@@ -59,7 +95,7 @@ setInterval(() => {
 }, 4000);
 
 
-// Sliding menu items
+// Sliding menu items --------------------------------------------------------
 function slideInItems() {
   let items = document.querySelectorAll('.items-box');
   let windowHeight = window.innerHeight;
