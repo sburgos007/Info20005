64aede84996081d68eb9057805b748649fa10561
saririvaii <snurfauziyya@student.unimelb.edu.au>
2023-06-01 16:38:53 +1000

    fixed hamburger menu

--- script.js
+++ script.js
@@ -2,10 +2,8 @@
 // Hamburger menu display on toggle ---------------------------------------
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
-const aboutbtn = document.querySelector('.about');
 
 menu_btn.addEventListener('click', function () {
-  aboutbtn.classList.toggle('active');
   menu_btn.classList.toggle('active');
   mobile_menu.classList.toggle('active');
 });
