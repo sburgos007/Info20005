a8759e56a5465495c372804494a52a2325dff79d
sburgos007 <sburgos@student.unimelb.edu.au>
2023-06-04 13:37:36 +1000

    Improved form validations on pg 5

--- script.js
+++ script.js
@@ -16,6 +16,137 @@ function isInputNumber(evt){
   }
 }
 
+var nameError = document.getElementById('noName');
+var phoneError = document.getElementById('noPhone');
+var addressError = document.getElementById('noAddress');
+var suburbError = document.getElementById('noSuburb');
+var postcodeError = document.getElementById('noPostcode');
+var cardnumError = document.getElementById('noCardnum');
+var expiryError = document.getElementById('noExpiry');
+var CVVError = document.getElementById('noCVV');
+
+
+function validateName(){
+  var name = document.getElementById('name').value;
+
+  if(name.length == 0){
+    nameError.innerHTML = '*Enter name';
+    return false
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHTML = '*Please write full name';
+    return false
+  }
+  nameError.innerHTML = '';
+  return true
+}
+
+function validatePhone(){
+  var phone = document.getElementById('phone').value;
+
+  if(phone.length == 0){
+    phoneError.innerHTML = '*Phone no. required';
+    return false
+  }
+  if(phone.length !== 10){
+    phoneError.innerHTML = '*10 digits required';
+    return false
+  }
+  phoneError.innerHTML = '';
+  return true
+}
+
+function validateAddress(){
+  var address = document.getElementById('address').value;
+
+  if(address.length == 0){
+    addressError.innerHTML = '*Address required';
+    return false
+  }
+  addressError.innerHTML = '';
+  return true
+}
+
+function validateSuburb(){
+  var suburb = document.getElementById('subDetails').value;
+
+  if(suburb.length == 0){
+    suburbError.innerHTML = '*Suburb required';
+    return false
+  }
+  suburbError.innerHTML = '';
+  return true
+}
+
+function validatePostcode(){
+  var postcode = document.getElementById('postDetails').value;
+
+  if(postcode.length == 0){
+    postcodeError.innerHTML = '*Postcode required';
+    return false
+  }
+  if(postcode.length !== 4){
+    postcodeError.innerHTML = '*Enter 4 digits';
+    return false
+  }
+  postcodeError.innerHTML = '';
+  return true
+}
+
+function validateCardnum(){
+  var cardnum = document.getElementById('cardnumDetails').value;
+
+  if(cardnum.length == 0){
+    cardnumError.innerHTML = '*Card No. required';
+    return false
+  }
+  if(cardnum.length !== 10){
+    cardnumError.innerHTML = '*Enter 10 digits';
+    return false
+  }
+  cardnumError.innerHTML = '';
+  return true
+}
+
+function validateExpiry(){
+  var expiry = document.getElementById('expiryDetails').value;
+
+  if(expiry.length == 0){
+    expiryError.innerHTML = '*Expiry required';
+    return false
+  }
+  if(!expiry.match(/^[0-9]*\/{1}[0-9]{2}$/)){
+    expiryError.innerHTML = '*DD/MM';
+    return false
+  }
+  expiryError.innerHTML = '';
+  return true
+}
+
+function validateCVV(){
+  var CVV = document.getElementById('CVVDetails').value;
+
+  if(CVV.length == 0){
+    CVVError.innerHTML = '*CVV required';
+    return false
+  }
+  if(CVV.length !== 3){
+    CVVError.innerHTML = '*Enter 3 digits';
+    return false
+  }
+  CVVError.innerHTML = '';
+  return true
+}
+
+
+
+function validateForm(){
+  if (!validateName() || !validatePhone() || !validateAddress() || !validateSuburb() || !validatePostcode() ||!validateCardnum() ||!validateExpiry()||!validateCVV()){
+    submitError.innerHTML = '*Please enter required fields ';
+    return false;
+  }
+}
+
 
 // About us header change -------------------------------------------------
 // Get the elements
@@ -78,8 +209,6 @@ function minus3() {
   document.getElementById('number3').stepDown();
 }
 
-
-
 // Adding pop up section for card input details----------------------------
 function showDetails() {
   var details = document.getElementById("cardDetails");
@@ -89,6 +218,16 @@ function showDetails() {
     details.style.display = "none";
   }
 }
+
+function showTime() {
+  var details = document.getElementById("timeSelect");
+  if (details.style.display === "none") {
+    details.style.display = "flex";
+  } else {
+    details.style.display = "none";
+  }
+}
+
 
 
 // Change background image every 5 seconds ---------------------------------
